14b0e1a30c0d2d1851fe5786669786f60bf68781
Sarvesh Yeutkar <98184185+saarv3sh7@users.noreply.github.com>
2026-08-24 18:18:53 +0530

    Update App.jsx

--- pizza-pos-frontend/src/App.jsx
+++ pizza-pos-frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import PosScreen from './components/PosScreen';
 import LoginScreen from './components/LoginScreen';
 import AdminDashboard from './components/AdminDashboard';
